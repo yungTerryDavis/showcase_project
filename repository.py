7c57213c6a8cb89a7b2617af6b7d5e0935bcec81
yungTerryDavis <chulkov.nikita01@gmail.com>
2025-10-10 13:26:47 +0300

    feat: Add class to repository and methods for 2-5 tasks

--- repository.py
+++ repository.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from sqlalchemy import cast, func, select, Numeric
 
 from database import Base, async_session_maker
-from models import PC
+from models import PC, Printer, Product, Laptop
 
 
 async def get_objects_count(model: type[Base]) -> int:
@@ -12,8 +12,37 @@ async def get_objects_count(model: type[Base]) -> int:
         return await session.scalar(stmt) or 0
 
 
-async def get_pcs_cheaper(value: Decimal):
-    async with async_session_maker() as session:
-        stmt = select(PC).where(cast(PC.price, Numeric) < value)
-        res = await session.scalars(stmt)
-        return res
+class SQLExRepository:
+    @classmethod
+    async def get_pcs_cheaper(cls, value: Decimal):
+        async with async_session_maker() as session:
+            stmt = select(PC).where(cast(PC.price, Numeric) < value)
+            return await session.scalars(stmt)
+
+    @classmethod
+    async def get_makers_of_type(cls, product_type: str):
+        async with async_session_maker() as session:
+            stmt = select(Product.maker).where(Product.type_ == product_type).distinct()
+            return await session.scalars(stmt)
+
+    @classmethod
+    async def get_laptops_more_expensive(cls, value: Decimal):
+        async with async_session_maker() as session:
+            stmt = select(Laptop).where(cast(Laptop.price, Numeric) > value)
+            return await session.scalars(stmt)
+
+    @classmethod
+    async def get_printers_colored(cls, colored: str):  # might be enum
+        async with async_session_maker() as session:
+            stmt = select(Printer).where(Printer.color == colored)
+            return await session.scalars(stmt)
+
+    @classmethod
+    async def get_pcs_cheaper_filter_cds(cls, cds: list[str], price: Decimal):
+        async with async_session_maker() as session:
+            stmt = (
+            select(PC)
+            .where(PC.cd.in_(cds))
+            .where(cast(PC.price, Numeric) < price)
+            )
+            return await session.scalars(stmt)
